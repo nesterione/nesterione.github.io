3686d8e592e64aebe166b24fbebb3936e8eed5d1
Igor Nesterenya <nesterione@gmail.com>
2015-08-12 19:41:02 +0300

    Add links

--- links/app.js
+++ links/app.js
@@ -1,28 +1,42 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-var app = angular.module('myApp', []);
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'controllersM'
+]);
+
+var controllersM = angular.module('controllersM', []);
 
 
-app.controller('MainCntl', function($scope, $http) {
+controllersM.controller('MainCntl', function($scope, $http, $route, $location, $routeParams) {
+ 
+ function getFilter() {
+	var str = $location.absUrl();
+	var params = str.split("#");
+	var vals = params[1].split("/");
+	//alert(vals[0]+" | "+vals[1]+" | "+vals[2]);
 	
-	/*$scope.links = [{
-   name: "Ссылка1",
-   description: "Описание ссылки 0",
-   categories: ["без категории", "машинное обучение"],
-   tags: [".NET",  "Ajax"]
- },
- {
-   name: "Ссылка2",
-   description: "Описание ссылки 1",
-   categories: ["без категории"],
-   tags: [ "ASP.NET", "Ajax"]
- }];*/
+	var returnValue = "";
+	if(vals[1]==="tag") {
+		returnValue = vals[2];
+	}
+	return returnValue;
+ }
+ 
+ //$scope.q1 = $routeParams.qqq;
+ //$scope.q1 = $route.current.params;
+ $scope.query = getFilter();//$location.absUrl();
+ 
+ 
  
  $scope.init = function () {
-    $http.get('links.json').
+    //$scope.query = $routeParams.qqq
+    
+	$http.get('rlinks.json').
               success(function(data, status, headers, config) {
                    $scope.links = data;
+				   debugger;
+				   //alert($routeParams.qqq);
               }).
               error(function(data, status, headers, config) {
                     alert("error data load, will be used test data");
